27068873415d4ab19a31b03b84ba3fbea7e5c2e7
HebrinJ <ashram28@gmail.com>
2023-09-05 18:43:31 +0400

    refactor: Логика закрытия модальных окон вынесена из modal-overlay и modal-wondow

--- src/components/modals/modal-overlay.jsx
+++ src/components/modals/modal-overlay.jsx
@@ -1,28 +1,9 @@
-import React from 'react'
 import style from './modal-overlay.module.css';
 
 function ModalOverlay(props) {
 
-    React.useEffect(() => {
-        document.addEventListener('keydown', handleKey);
-        
-        return () => {
-            document.removeEventListener('keydown', handleKey);
-        }        
-    }, [])
-
-    const handleClick = (e) => {
-        props.handleCloseModal();
-    }
-
-    const handleKey = (e) => {
-        if(e.key === 'Escape') {
-            props.handleCloseModal();
-        }
-    }
-
     return (
-        <div className={style.overlay} onClick={handleClick}>
+        <div className={style.overlay} onClick={props.handleCloseModal}>
 
         </div>
     )
--- src/components/modals/modal-window.jsx
+++ src/components/modals/modal-window.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
+import React from 'react';
 import style from './modal-window.module.css';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import PropTypes from 'prop-types';
@@ -6,6 +7,20 @@ import { ingredientPropType } from '../../utils/prop-types';
 import ModalOverlay from './modal-overlay';
 
 function ModalWindow(props) {     
+
+    React.useEffect(() => {
+        document.addEventListener('keydown', handleKey);
+        
+        return () => {
+            document.removeEventListener('keydown', handleKey);
+        }        
+    }, [])
+
+    const handleKey = (e) => {
+        if(e.key === 'Escape') {
+            props.handleCloseModal();
+        }
+    }
 
     return ReactDOM.createPortal((        
         <>
